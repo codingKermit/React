99747b6b5aae6900f7fd407eb8cdc43a9a9d7776
codingKermit <woojw6012@gmail.com>
2024-06-20 00:30:08 +0900

    coins

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
 
-  const [toDo, setToDo] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const [toDos, setToDos] = useState([]);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(()=>{
+    // fetch("https://api.coinpaprika.com/v1/tickers")
+    // .then((res)=> res.json())
+    // .then((json)=>console.log(json));
+
+    axios.get("https://api.coinpaprika.com/v1/tickers")
+    .then((res)=>{
+      console.log(res)
+      setCoins(res.data);
+      setLoading(false);
+    })
+    .catch((err)=>{console.log(err)});
 
 
-  const onChange = (e) => setToDo(e.target.value);
 
-  const onSubmit = (e) => {
-    e.preventDefault(); 
+  },[])
 
-    if(toDo === ""){
-      return;
-    }
-
-    setToDos((currentArray)=>[toDo, ...currentArray]);
-
-    setToDo("");
-
-  }
-
-  console.log(toDos);
-  
   return (
     <div className="App">
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input type='text' placeholder='Write your to do...' value={toDo} onChange={onChange}/>
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item, index)=><li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (<strong>Loading...</strong>) : 
+      (
+        <div>
+          <select>
+            {coins.map((coin, index)=>{
+              return(
+                <option key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>
+              )
+            })}
+          </select>
+          <ul>
+            {coins.map((coin,index)=>{
+              return(
+                <li key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
